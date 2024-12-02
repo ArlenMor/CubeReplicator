1ee28741c7fa0807c59b7d219a60e1d36c6fcb0e
Arlen <Demonik4053@gmail.com>
2024-12-02 18:09:38 +0700

    Переножу AddExplosionForce в FixedUpdate

--- Assets/Scripts/Explosioner.cs
+++ Assets/Scripts/Explosioner.cs
@@ -3,12 +3,50 @@ using UnityEngine;
 
 public class Explosioner : MonoBehaviour
 {
-    [SerializeField, Range(1, 10)] private float _radius;
-    [SerializeField, Range(100,500)] private float _explosionForce;
+    [SerializeField, Range(5, 30)] private float _radius;
+    [SerializeField, Range(100, 500)] private float _explosionForce;
+
+    private bool _explodeThisFrame = false;
+    private Transform _centerExplosion;
+    private List<Rigidbody> _explosiveObjects;
+
+    private void FixedUpdate()
+    {
+        if (_explodeThisFrame)
+        {
+            if(_centerExplosion != null)
+            {
+                for (int i = 0; i < _explosiveObjects.Count; i++)
+                {
+                    if (_explosiveObjects[i] != null)
+                        _explosiveObjects[i].AddExplosionForce(_explosionForce, _centerExplosion.position, _radius);
+                }
+            }
+
+            _explodeThisFrame = false;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (_centerExplosion != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(_centerExplosion.position, _radius);
+        }
+    }
 
     public void Explode(Transform centerExplosion, List<Rigidbody> explosiveObjects)
     {
-        for (int i = 0; i < explosiveObjects.Count; i++)
-            explosiveObjects[i].AddExplosionForce(_explosionForce, centerExplosion.position, _radius);
+
+        if (centerExplosion != null && explosiveObjects != null)
+        {
+            _explodeThisFrame = true;
+            _centerExplosion = centerExplosion;
+            _explosiveObjects = explosiveObjects;
+        }
+
+        /*for (int i = 0; i < explosiveObjects.Count; i++)
+            explosiveObjects[i].AddExplosionForce(_explosionForce, centerExplosion.position, _radius);*/
     }
 }
